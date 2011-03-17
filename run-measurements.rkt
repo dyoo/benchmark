d48f30538840ce8bd3e4871a5123c904a7f0cfd7
Danny Yoo <dyoo@cs.wpi.edu>
2011-03-17 17:34:58 -0400

    adjusting the test runner so I can easily run a subset of platforms or benchmarks

--- run-measurements.rkt
+++ run-measurements.rkt
@@ -10,28 +10,65 @@
 (require (prefix-in simulator: "platforms/js-sicp-5-5-simulator/runner.rkt"))
 (require (prefix-in js-vm: "platforms/js-vm/runner.rkt"))
 
-
 (define-runtime-path this-path ".")
 
-(define programs '(("programs/test" test)
-                   ("programs/gauss" gauss)
-		   ("programs/gauss-iter" gauss-iter)
-		   ("programs/cpstack" cpstack)
-		   ("programs/tak" tak)
-		   ("programs/conform" conform)
-		   ))
+
+(define-struct platform (name runner))
+(define all-platforms (list (make-platform "racket" racket:run)
+                            (make-platform "simulator" simulator:run)
+                            (make-platform "js-vm" js-vm:run)
+                            (make-platform "browser" browser:run)))
+
+(define (find-platform name)
+  (let loop ([platforms all-platforms])
+    (cond
+      [(empty? platforms)
+       (error 'find-platform "unknown platform ~s" name)]
+      [(string=? name (platform-name (first platforms)))
+       (first platforms)]
+      [else
+       (loop (rest platforms))])))
+
+(define-struct program (dir name))
+
+(define all-programs (list (make-program "programs/test" 'test)
+                           (make-program "programs/gauss" 'gauss)
+                           (make-program "programs/gauss-iter" 'gauss-iter)
+                           (make-program "programs/cpstack" 'cpstack)
+                           (make-program "programs/tak" 'tak)
+                           (make-program "programs/conform" 'conform)))
+
+(define (find-program name)
+  (let loop ([programs all-programs])
+    (cond
+      [(empty? programs)
+       (error 'find-program "unknown program ~s" name)]
+      [(string=? name (symbol->string (program-name (first programs))))
+       (first programs)]
+      [else
+       (loop (rest programs))])))
 
 
-(for ([p programs])
-  (let ([dir (first p)]
-	[module-name (second p)])
-    (printf "Running ~a benchmark...\n" module-name)
-    (parameterize ([current-directory this-path])
-      (printf "    racket...\n")
-      (save-measurement! (racket:run dir module-name))
-      #;(printf "    simulator...\n")
-      #;(save-measurement! (simulator:run dir module-name))
-      (printf "    js-vm...\n")
-      (save-measurement! (js-vm:run dir module-name))
-      (printf "    browser...\n")
-      (save-measurement! (browser:run dir module-name)))))
+(define-values (programs platforms)
+  (cond
+    [(> (vector-length (current-command-line-arguments)) 0)
+     (let ([platform (find-platform 
+                      (vector-ref (current-command-line-arguments) 0))])
+       (cond [(> (vector-length (current-command-line-arguments)) 1)
+              (let ([programs
+                     (map find-program 
+                          (rest (vector->list (current-command-line-arguments))))])
+                (values programs (list platform)))]
+             [else
+              (values all-programs (list platform))]))]
+    [else 
+     (values all-programs all-platforms)]))
+
+
+
+(for ([program programs])
+  (printf "Running ~a benchmark...\n" (program-name program))
+  (parameterize ([current-directory this-path])
+    (for ([platform platforms])
+      (printf "    ~a...\n" (platform-name platform))
+      (save-measurement! ((platform-runner platform) (program-dir program) (program-name program))))))
