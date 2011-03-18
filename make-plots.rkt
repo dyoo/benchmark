eb77d59e2eb5cc62ebd5c0b9a3509e8541ed9df4
Danny Yoo <dyoo@cs.wpi.edu>
2011-03-18 15:39:46 -0400

    initial graphs

--- make-plots.rkt
+++ make-plots.rkt
@@ -15,6 +15,10 @@
          racket/pretty
          "measurement-struct.rkt")
 
+
+(define plot-width 400)
+(define plot-height 400)
+
 (define plots-path "data/plots")
 
 
@@ -35,11 +39,26 @@ EOF
   (let ([graph-text #<<EOF
     <div>
     <h1>~a</h1>
-    <iframe src="~a.html" width="300" height="200"></iframe>
+    <iframe src="~a.html" width="~a" height="~a"></iframe>
+    <p><small>(smaller is better)</small></p>
+    <pre>
+~a
+    </pre>
     </div>
 EOF
                     ])
-    (format graph-text name name)))
+    (format graph-text
+            name name
+            plot-width
+            plot-height
+            (string-join (map (lambda (p)
+                                (format "Day ~a: ~a took ~a times longer than racket"
+                                        (analyzed-point-day p)
+                                        (analyzed-point-platform p)
+                                        (analyzed-point-times-slower p)))
+                              (second (assoc name analyzed-points)))
+                         "\n")
+            )))
 
 
 (define-runtime-path measurements "data/measurements")
@@ -145,11 +164,15 @@ EOF
           pts))
   
 
-;;(define cluster-datapoints-by-day (cluster data-points data-point-date))
 
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; This analysis computes time relative to Racket raw performance, for each test,
+;; on each day.
+;;
 
+;; (listof number) -> number
 (define (average nums)
   (/ (apply + nums)
      (length nums)))
@@ -158,15 +181,19 @@ EOF
 ;; (listof data-point) -> (listof (list [test-name string] (listof analyzed-point)))
 (define (analyze)
   (for/list ([test (unique-strings (map data-point-program data-points))])
-    (list test (analyze-by-test (filter-points-by-program data-points test)))))
+    (list test 
+          (sort (analyze-by-test (filter-points-by-program data-points test))
+                analyzed-point<?))))
 
 
 ;; (listof data-point) -> (listof ...)
 (define (analyze-by-test points)
-  (let ([clustered-by-day (cluster points data-point-date)])
+  (let* ([clustered-by-day (cluster points data-point-date)])
     (apply append
            (for/list ([day (in-hash-keys clustered-by-day)])
              (analyze-single-day day (hash-ref clustered-by-day day))))))
+
+  
 
 
 ;; number points -> (listof analyzed-point)
@@ -184,48 +211,87 @@ EOF
                      (cons (analyzed-point-times-slower p)
                            (hash-ref ht (analyzed-point-platform p) '()))))
         (for/list ([key (in-hash-keys ht)])
-          (make-analyzed-point day key (average (hash-ref ht key))))))
+          (make-analyzed-point (add1 day) key (average (hash-ref ht key))))))
     (normalize (apply append data-per-host))))
 
 ;; number points -> (listof analyzed-point)
 (define (analyze-single-day-and-host day points)
-  (pretty-print points)
   ;; First, find how long it took for racket.
   (let ([time-for-racket
          (average (map data-point-time 
                        (filter-points-by-key points data-point-platform "racket")))])
-    (printf "Time for racket: ~s\n" time-for-racket)
+    #;(printf "Time for racket: ~s\n" time-for-racket)
     ;; Then, for every other platform, restate the data in terms of times slower than racket. 
     (for/list ([platform (remove "racket" (unique-strings (map data-point-platform points)))])
       (let ([platform-points (filter-points-by-key points data-point-platform platform)])
         (let ([times-slower
                (/ (average (map data-point-time platform-points))
                   time-for-racket)])
-          (printf "~s: ~s times slower than racket\n" 
+          #;(printf "~s: ~s times slower than racket\n" 
                   platform
                   times-slower)
-          (make-analyzed-point day platform #;time-for-racket times-slower))))))
+          (make-analyzed-point (add1 day) platform times-slower))))))
 
-(define-struct analyzed-point (day platform #;time-for-racket times-slower)
+(define-struct analyzed-point (day platform times-slower)
   #:transparent)
 
 
-
+(define (analyzed-point<? p1 p2)
+  (cond
+    [(< (analyzed-point-day p1)
+        (analyzed-point-day p2))
+     #t]
+    [(> (analyzed-point-day p1)
+        (analyzed-point-day p2))
+     #f]
+    [else
+     (string<? (analyzed-point-platform p1)
+               (analyzed-point-platform p2))]))
 
 
                                                      
 
+(define analyzed-points (analyze))
 
-
-
+(define (clone-first l)
+  (cons
+   (car l) l))
 
 (define (make-plot name)
-  (let* ([points (filter-points-by-program data-points name)]
-         [clustered-by-day (cluster points data-point-date)]
-         [platform-names (remove "racket" (unique-strings (map data-point-platform points)))]
+  (let* ([points (second (assoc name analyzed-points))]
+         [platform-names (remove "racket" (unique-strings (map analyzed-point-platform points)))]
          [colors (take ALL-COLORS (length platform-names))]
-         [max-days (apply max (map data-point-date points))]
-         [max-times (apply max (map data-point-time points))]
+         [min-days (apply min (map analyzed-point-day points))]
+         [max-days (apply max (map analyzed-point-day points))]
+         [min-times-slower (apply min (map analyzed-point-times-slower points))]
+         [max-times-slower (apply max (map analyzed-point-times-slower points))]
+         [point-text (string-join 
+                      (map (lambda (platform-name)
+                             (let* ([platform-points
+                                     (filter (lambda (p) (string=? (analyzed-point-platform p) platform-name))
+                                             points)]
+                                    [normalized-platform-days
+                                     (cons 0
+                                           (map (lambda (x)
+                                                  (inexact->exact (floor (* 100.0
+                                                                            (/ (analyzed-point-day x)
+                                                                               max-days)))))
+                                                platform-points))]
+                                    [normalized-times-slower
+                                     (clone-first (map (lambda (x)
+                                                         (inexact->exact (floor (* 100.0
+                                                                                   (/ (analyzed-point-times-slower x)
+                                                                                      max-times-slower)))))
+                                                       platform-points))])
+                               (string-append (string-join
+                                               (map number->string normalized-platform-days)
+                                               ",")
+                                              "|"
+                                              (string-join
+                                               (map number->string normalized-times-slower)
+                                               ","))))
+                           platform-names)
+                      "|")]
          [text #<<EOF
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
@@ -248,9 +314,9 @@ EOF
           onsubmit="this.action = 'https://chart.googleapis.com/chart?chid=' + (new Date()).getMilliseconds(); return true;">
       <input type='hidden' name='cht' value='lxy'/>
       <input type='hidden' name='chtt' value=~s/>
-      <input type='hidden' name='chs' value='300x200'/>
+      <input type='hidden' name='chs' value='~ax~a'/>
       <input type='hidden' name='chxt' value='x,y'/>
-      <input type='hidden' name='chxr' value='0,0,~a|1,0,~a'>
+      <input type='hidden' name='chxr' value='0,~a,~a|1,~a,~a'>
       <input type='hidden' name='chco' value="~a">
       <input type='hidden' name='chdl' value="~a">
       <input type='hidden' name='chd' value='t:~a'/>
@@ -260,20 +326,26 @@ EOF
 </html>
 EOF
               ])
-    (format text name
+    (format text
+            name
+            plot-width
+            plot-height
+            min-days
             max-days
-            max-times
+            min-times-slower
+            max-times-slower
             (string-join colors ",")
             (string-join platform-names "|")
-            '() ;; points
+            point-text
             )))
 
 
-(define ALL-COLORS '("FF0000" "00FF00" "0000FF" "0FF0000" "000FF0" "F0F000" "F00F00"))
+(define ALL-COLORS '("FF0000" "00FF00" "0000FF" "0FF0FF"))
 
 
 
 (define (normalize numbers)
+  (printf "I see numbers: ~s" numbers)
   (let* ([min-val (apply min numbers)]
          [max-val (apply max numbers)]
          [width (- max-val min-val)])
@@ -292,7 +364,7 @@ EOF
     (display (format template 
                      (apply string-append
                             (map make-graph-link
-                                 (take all-program-names 1))))
+                                 all-program-names)))
              op))
   #:exists 'replace)
 
